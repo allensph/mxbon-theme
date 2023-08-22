e5df83ae67f2ec8f78ea58b7c7f71e52146d40ae
allensph <allensph@gmail.com>
2023-08-22 17:28:25 +0800

    page-home.php init

---
+++ public/wp-content/themes/tailpress-master/page-home.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<i class="fa-solid fa-location-dot"></i>
+
+<section class="product-categories">
+    <div class="section-title">
+        產品分類
+        <span class="sub">products</span>
+    </div>
+</section>
+
+<?php
+    get_footer();
