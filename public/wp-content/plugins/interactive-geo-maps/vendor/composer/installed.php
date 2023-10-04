7bd67e8312d4734176c562df6e53412b766597af
allensph <allensph@gmail.com>
2023-10-04 15:18:04 +0800

    Interractibe Geo Maps plugin install

---
+++ public/wp-content/plugins/interactive-geo-maps/vendor/composer/installed.php
@@ -0,0 +1,54 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carmoreira/interactive-geo-maps',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '96ed117d1ededd00683bca4695735eef5bb626b3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../build',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'carmoreira/interactive-geo-maps' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '96ed117d1ededd00683bca4695735eef5bb626b3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../build',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e9e3b37d165a881326991a672af0269a6e2b2cda',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'hassankhan/config' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'reference' => '8e1c07f4fdc2b986b8a781f0dcba155af2a579b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hassankhan/config',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'saltus/framework' => array(
+            'pretty_version' => 'dev-dev',
+            'version' => 'dev-dev',
+            'reference' => '481fac594b73179faadf253886ee60d3a6d4c412',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../saltus/framework',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
