0aacb395bc6a3307dd3afa5bb3ab31ace0f95ac3
allensph <allensph@gmail.com>
2024-01-24 15:00:00 +0800

    Add GA4 & Gtag to header.php

--- public/wp-content/themes/tailpress-master/header.php
+++ public/wp-content/themes/tailpress-master/header.php
@@ -7,9 +7,30 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5355BJX');</script>
+	<!-- End Google Tag Manager -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5355BJX "
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 </head>
 
 <body <?php body_class( 'bg-white text-gray-900 antialiased' ); ?>>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LX0VFYELHL"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-LX0VFYELHL');
+</script>
 
 <?php do_action( 'tailpress_site_before' ); ?>
 
